75b00642ae4eb8eae7506dc966037cc55c497b7f
dongh38 <Dh13623652058>
2017-05-18 19:30:06 +0800

    fix bug.

--- rpc-akka/src/main/java/com/asterisk/rpc/akka/Greeter.java
+++ rpc-akka/src/main/java/com/asterisk/rpc/akka/Greeter.java
@@ -1,12 +1,19 @@
 package com.asterisk.rpc.akka;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.actor.Inbox;
+import akka.actor.Props;
 import akka.dispatch.ExecutionContexts;
 import akka.dispatch.Futures;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
 import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author dongh38@ziroom.com
@@ -21,17 +28,17 @@ public class Greeter extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        Future<String> future = Futures.successful("foo");
+        ActorRef myUnTypedActor = getContext().actorOf(Props.create(MyUnTypedActor.class));
         return receiveBuilder()
                 .matchEquals(Msg.GREET, m -> {
                     System.out.println("Hello world");
                     LOGGER.info("sender is:{} ,receiver is:{}",self(),sender());
-                    sender().tell("hehe",self());
+                    myUnTypedActor.tell("hehe",self());
 //                    sender().tell(Msg.DONE, self());
                 })
                 .matchEquals("hello,I'm inbox", msg -> {
                     System.out.println("ok I have received .." + msg);
-                    sender().tell("hehe",self());
+                    myUnTypedActor.tell("hehe",self());
 
                 }).matchAny(msg -> System.out.println("unknown type message? who is you?" + msg)
         )
--- rpc-akka/src/main/java/com/asterisk/rpc/akka/MyUnTypedActor.java
+++ rpc-akka/src/main/java/com/asterisk/rpc/akka/MyUnTypedActor.java
@@ -18,8 +18,18 @@ public class MyUnTypedActor extends AbstractActor {
 
     private final LoggingAdapter LOGGER = Logging.getLogger(getContext().getSystem(), this);
 
+    private final ActorRef child = getContext().actorOf(Props.empty(),"target");
+
+    private ActorRef lastSender = getContext().system().deadLetters();
+
+    public MyUnTypedActor() {
+        getContext().watch(child);
+    }
+
     @Override
     public Receive createReceive() {
+
+
         return receiveBuilder()
                 .matchEquals(Greeter.Msg.DONE, s -> {
                     LOGGER.info("Received String message:{}", s);
@@ -28,17 +38,22 @@ public class MyUnTypedActor extends AbstractActor {
                 .matchEquals("hehe", s -> {
                     System.out.println("hello greeter, your message is:" + s);
                 })
+                .matchEquals("kill",s -> {
+                    getContext().stop(child);
+                    lastSender = getSender();
+                })
+                .match(Terminated.class, t -> t.actor().equals(child),t -> {
+                    lastSender.tell("finished",getSelf());
+                })
                 .build();
     }
 
     @Override
     public void preStart() throws Exception {
-        ActorSystem system = ActorSystem.create("asterisk-rpc");
         ActorRef greeter = getContext().actorOf(Props.create(Greeter.class), "greeter");
         LOGGER.info("greeter path:{}, sender is:{}", greeter.path(), self());
-        Inbox inbox = Inbox.create(system);
+        Inbox inbox = Inbox.create(getContext().system());
         inbox.send(greeter, "hello,I'm inbox");
-        Timeout timeout = new Timeout(Duration.create(5, TimeUnit.SECONDS));
-        Future<Object> future = Patterns.ask(greeter, Greeter.Msg.GREET, timeout);
+        inbox.send(greeter, Greeter.Msg.GREET);
     }
 }
